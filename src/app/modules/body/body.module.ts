64e29dd3ce9ca85aab678e209d968a49f38228d7
unknown <shirley.aguilar.agui@gmail.com>
2023-01-24 14:55:20 -0500

    add styles of header

--- src/app/modules/body/body.module.ts
+++ src/app/modules/body/body.module.ts
@@ -1,12 +1,31 @@
 import { NgModule} from "@angular/core";
 import { CommonModule} from "@angular/common";
-import {BodyComponent} from "./components/body.component";
-import {BodyRoutingModule} from "./body-routing.module";
+import { BodyComponent} from "./components/body.component";
+import { BodyRoutingModule} from "./body-routing.module";
 import { HeaderComponent } from './components/header/header.component';
+import { NgOptimizedImage} from "@angular/common";
+import {RouterModule} from "@angular/router";
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ContactInformationComponent } from './components/contact-information/contact-information.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
-  declarations: [BodyComponent, HeaderComponent],
-  imports: [CommonModule, BodyRoutingModule],
+  declarations: [
+    BodyComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ContactInformationComponent,
+    SkillsComponent],
+  imports: [
+    CommonModule,
+    BodyRoutingModule,
+    NgOptimizedImage,
+    RouterModule
+  ],
   providers: []
 })
 
--- src/app/modules/body/components/header/header.component.ts
+++ src/app/modules/body/components/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  HostListener,
+} from "@angular/core";
 
 @Component({
   selector: 'app-header',
@@ -7,6 +10,24 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   sections = ["home", "about", "skills", "portfolio", "contact me"];
+  expansionPanel = false;
+
   constructor(){}
 
+  ngOnInit(): void {
+    if (window.innerWidth > 700) {
+      this.expansionPanel = true;
+    }
+  }
+
+  @HostListener("window:resize", ["$event"])
+  onResize(event: { target: { innerWidth: number } }) {
+    this.expansionPanel =  event.target.innerWidth > 700
+  }
+
+  showNav(list:HTMLElement):void {
+
+   list.classList.toggle("show-items")
+
+  }
 }
