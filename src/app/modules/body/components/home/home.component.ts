c0b08e893dca16d772c4904dfd8da616ce1d0d47
unknown <shirley.aguilar.agui@gmail.com>
2023-02-05 14:41:00 -0500

    add animation for the first page

--- src/app/modules/body/components/home/home.component.ts
+++ src/app/modules/body/components/home/home.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import Typewriter from 't-writer.js';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild('asName') asName: ElementRef;
+
+  ngAfterViewInit(): void {
+    this.initEffect();
+  }
+  ngOnInit(): void {
+
+  }
+
+
+
+  initEffect = () => {
+    const target = this.asName.nativeElement;
+
+    const writer = new Typewriter(target, {
+      loop: true,
+      typeColor: '#FB2576'
+    })
+
+    writer
+      .changeCursorColor('white')
+      .type('Shirley Aguilar')
+      .rest(5000)
+      .start()
+
+  }
+
+
 
 }
